2612715bbbbddb98c7f84bbba577d7de2622bb33
GUILHERME HENRIQUE ARAUJO <guilhermeh.araujo1203@gmail.com>
2025-10-16 20:40:49 -0300

    Hospede.java

---
+++ package br.com.hotel.entidades;  import java.io.Serializable;  public class Hospede implements Serializable {     private int id;     private String nome;     private String cpf;     /Hospede.java
@@ -0,0 +1,10 @@
+package br.com.hotel.entidades;
+
+import java.io.Serializable;
+
+public class Hospede implements Serializable {
+    private int id;
+    private String nome;
+    private String cpf;
+    // Getters e Setters...
+}
