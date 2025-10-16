e0a1b80d67dd2cfab32f7e968f43159e5431963f
GUILHERME HENRIQUE ARAUJO <guilhermeh.araujo1203@gmail.com>
2025-10-16 20:43:56 -0300

    Quarto.java

---
+++ Quarto.java
@@ -0,0 +1,12 @@
+package br.com.hotel.entidades;
+
+import java.io.Serializable;
+
+public class Quarto implements Serializable {
+    private int id;
+    private int numero;
+    private String tipo;
+    private String status;
+    private double precoDiaria;
+    // Getters e Setters...
+}
